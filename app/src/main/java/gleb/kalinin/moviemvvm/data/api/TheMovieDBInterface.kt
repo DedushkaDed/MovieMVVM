7dde00f74993f1d76312d4ad10ae55f16e7d13df
dedushkaded <glebstuck@yandex.ru>
2020-05-10 15:47:22 +0500

    Подготовка MainActivity для отображения популярных фильмов REST API

--- app/src/main/java/gleb/kalinin/moviemvvm/data/api/TheMovieDBInterface.kt
+++ app/src/main/java/gleb/kalinin/moviemvvm/data/api/TheMovieDBInterface.kt
@@ -1,15 +1,19 @@
 package gleb.kalinin.moviemvvm.data.api
 
 import gleb.kalinin.moviemvvm.data.vo.MovieDetails
+import gleb.kalinin.moviemvvm.data.vo.MovieResponse
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface TheMovieDBInterface {
-
     // https://api.themoviedb.org/3/movie/popular?api_key=b764966fe6510611a5336b30098acde1&language=en-US&page=1 - Популярные фильмы
     // https://api.themoviedb.org/3/movie/338762?api_key=b764966fe6510611a5336b30098acde1&language=en-US - Информация о одном фильме
     // https://api.themoviedb.org/3/ - BASE_URL
+
+    @GET("movie/popular")
+    fun getPopularMovie (@Query ("page") page: Int): Single<MovieResponse>
 
     @GET("movie/{movie_id}")
     fun getMovieDetails (@Path("movie_id") id: Int) : Single<MovieDetails>  // Single - rxJava
